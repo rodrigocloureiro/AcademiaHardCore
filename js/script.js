185e76fc5d0b7c9cde82bdde15de8a54c5fc40bf
rodrigocloureiro <110643385+rodrigocloureiro@users.noreply.github.com>
2022-10-17 15:57:44 -0300

    versão final

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 // var header = document.getElementsByClassName('header');
 var header = document.querySelector('.header');
-var menu = document.querySelector('.menu');
 console.log(header);
+
+var menu = document.querySelector(".menu");
+var btnMenu = document.querySelector(".btn-menu");
 
 window.addEventListener('scroll', function(){
 
     console.log("ROLOU");
+
+    menu.classList.remove("menu-open");
 
     var scrollPixels = window.scrollY;
 
@@ -13,20 +17,14 @@ window.addEventListener('scroll', function(){
     if(scrollPixels > 150) {
         // console.log("OK");
         header.classList.add('header-fixed');
-        menu.classList.add('menu-fixed');
     }
     else {
         // console.log("AINDA NÃO");
         header.classList.remove('header-fixed');
-        menu.classList.remove('menu-fixed');
     }
 
 });
 
-var btnMenu = document.querySelector(".btn-menu");
-var menu = document.querySelector(".menu");
-
-btnMenu.addEventListener("click", function() {
+btnMenu.addEventListener('click', function() {
     menu.classList.toggle("menu-open");
-    this.classList.toggle("clicado"); // ou btnMenu.classList.toggle("clicado");
 });
